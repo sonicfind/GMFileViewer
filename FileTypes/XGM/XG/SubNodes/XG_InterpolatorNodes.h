c5f9eab0e10180d430114153b8ff7845ae956bbd
sonicfind <sonicfind2@gmail.com>
2023-01-04 10:01:42 -0600

    Interpolators: replace two bool template parameters with an enum

--- FileTypes/XGM/XG/SubNodes/XG_InterpolatorNodes.h
+++ FileTypes/XGM/XG/SubNodes/XG_InterpolatorNodes.h
@@ -3,9 +3,17 @@
 #include "xgTime.h"
 #include "XGArray.h"
 
-template <class T, bool useTIMES = false, bool useTARGETS = false>
+enum class InterpolatorType
+{
+	BASE,
+	TIMED,
+	TARGETED,
+};
+
+template <class T, InterpolatorType INTERPOLATION = InterpolatorType::BASE>
 class XG_InterpolatorNode : public XG_SubNode
 {
+	static_assert(INTERPOLATION >= InterpolatorType::BASE && INTERPOLATION <= InterpolatorType::TARGETED);
 protected:
 	uint32_t m_type = 0;
 	XGArray<float> m_times;
@@ -18,7 +26,7 @@ public:
 	{
 		PString::ReadNamedValue("type", m_type, input);
 
-		if constexpr (useTIMES)
+		if constexpr (INTERPOLATION >= InterpolatorType::TIMED)
 		{
 			PString::CheckForString("times", input);
 			XGArrayLoader::loadElements(m_times, input);
@@ -27,7 +35,7 @@ public:
 		PString::CheckForString("keys", input);
 		XGArrayLoader::loadElements(m_keys, input);
 
-		if constexpr (useTARGETS)
+		if constexpr (INTERPOLATION == InterpolatorType::TARGETED)
 		{
 			PString::CheckForString("targets", input);
 			XGArrayLoader::loadElements(m_targets, input);
@@ -40,8 +48,8 @@ public:
 class xgVec3Interpolator     : public XG_InterpolatorNode<Eigen::Vector3f> {};
 class xgQuatInterpolator     : public XG_InterpolatorNode<Eigen::Quaternionf> {};
 
-class xgVertexInterpolator   : public XG_InterpolatorNode<XGArray<Eigen::Vector3f>, true, true> {};
-class xgNormalInterpolator   : public XG_InterpolatorNode<XGArray<Eigen::Vector3f>, true, true> {};
-class xgTexCoordInterpolator : public XG_InterpolatorNode<XGArray<Eigen::Vector2f>, true, true> {};
+class xgVertexInterpolator   : public XG_InterpolatorNode<XGArray<Eigen::Vector3f>, InterpolatorType::TARGETED> {};
+class xgNormalInterpolator   : public XG_InterpolatorNode<XGArray<Eigen::Vector3f>, InterpolatorType::TARGETED> {};
+class xgTexCoordInterpolator : public XG_InterpolatorNode<XGArray<Eigen::Vector2f>, InterpolatorType::TARGETED> {};
 
-class xgShapeInterpolator    : public XG_InterpolatorNode<VertexList, true> {};
+class xgShapeInterpolator    : public XG_InterpolatorNode<VertexList, InterpolatorType::TIMED> {};
