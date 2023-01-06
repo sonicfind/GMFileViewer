3351b177472dc08e99c0540d935ee3f87d1fbf47
sonicfind <sonicfind2@gmail.com>
2023-01-06 13:20:17 -0600

    XG_SubNode: add requirement to confirm template is a subNode type

--- FileTypes/XGM/XG/SubNodes/XG_SubNode.h
+++ FileTypes/XGM/XG/SubNodes/XG_SubNode.h
@@ -8,34 +8,34 @@ class XG_SubNode
 public:
 	using XGVectElement = std::pair<std::string, std::unique_ptr<XG_SubNode>>;
 
-	template <typename T = XG_SubNode>
-	static T* searchForNode(const char*& input, const std::vector<XGVectElement>& nodes)
+	template <typename NodeType = XG_SubNode, typename = std::enable_if<std::is_base_of<XG_SubNode, NodeType>::value>>
+	static NodeType* searchForNode(const char*& input, const std::vector<XGVectElement>& nodes)
 	{
 		std::string_view name = PString::Read(input);
 		for (const auto& node : nodes)
 			if (node.first == name)
-				return static_cast<T*>(node.second.get());
+				return static_cast<NodeType*>(node.second.get());
 		return nullptr;
 	}
 
 protected:
-	template <typename T>
-	static void BindNodes(std::vector<T*>& nodeList, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
+	template <typename NodeType = XG_SubNode, typename = std::enable_if<std::is_base_of<XG_SubNode, NodeType>::value>>
+	static void BindNodes(std::vector<NodeType*>& nodeList, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
 	{
 		while (PString::CheckForString_nothrow(inputString, input))
 		{
-			nodeList.push_back(searchForNode<T>(input, nodes));
+			nodeList.push_back(searchForNode<NodeType>(input, nodes));
 			PString::CheckForString(outputString, input);
 		}
 	}
 
-	template <typename T>
-	static bool BindNode_optional(T*& node, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
+	template <typename NodeType = XG_SubNode, typename = std::enable_if<std::is_base_of<XG_SubNode, NodeType>::value>>
+	static bool BindNode_optional(NodeType*& node, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
 	{
 		if (!PString::CheckForString_nothrow(inputString, input))
 			return false;
 
-		node = searchForNode<T>(input, nodes);
+		node = searchForNode<NodeType>(input, nodes);
 
 		if (!PString::CheckForString_nothrow(outputString, input))
 			throw "Output string not matched";
@@ -43,8 +43,8 @@ protected:
 		return true;
 	}
 
-	template <typename T>
-	static void BindNode(T*& node, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
+	template <typename NodeType = XG_SubNode, typename = std::enable_if<std::is_base_of<XG_SubNode, NodeType>::value>>
+	static void BindNode(NodeType*& node, std::string_view inputString, std::string_view outputString, const char*& input, const std::vector<XGVectElement>& nodes)
 	{
 		if (!BindNode_optional(node, inputString, outputString, input, nodes))
 			throw "Input string not matched";
