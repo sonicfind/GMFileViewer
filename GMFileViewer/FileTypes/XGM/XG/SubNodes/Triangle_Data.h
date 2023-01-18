e01ea7ee8e78a797bcc7c9d5ddd66c61c73c1457
sonicfind <sonicfind2@gmail.com>
2023-01-18 14:23:14 -0600

    Triangle_Data: templatize draw()
    + Ensures that `if (primType)` only needs to be queried once between all data objects in xgDagMesh

--- GMFileViewer/FileTypes/XGM/XG/SubNodes/Triangle_Data.h
+++ GMFileViewer/FileTypes/XGM/XG/SubNodes/Triangle_Data.h
@@ -50,12 +50,13 @@ public:
 		m_data.reserve_and_fill(file);
 	}
 
-	void draw(uint32_t primType) const
+	template <uint32_t PRIMTYPE>
+	void draw() const
 	{
 		if (m_data.isEmpty())
 			return;
 
-		if (primType == 4)
+		if constexpr (PRIMTYPE == 4)
 		{
 			for (size_t i = 0; i < m_data.getSize();)
 			{
@@ -66,13 +67,14 @@ public:
 				i += numVerts;
 			}
 		}
-		else if (primType == 5)
+		else if constexpr (PRIMTYPE == 5)
 		{
 			uint32_t vertIndex = m_data.front();
 			for (size_t i = 1; i < m_data.getSize(); ++i)
 			{
+				const uint32_t count = m_data[i];
 				/* Draw Array Here */
-				vertIndex += m_data[i];
+				vertIndex += count;
 			}
 		}
 	}
