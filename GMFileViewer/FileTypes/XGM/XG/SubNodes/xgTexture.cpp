1081ecdb71de842ded7bc49f43a32838dc10569e
sonicfind <sonicfind2@gmail.com>
2023-01-19 12:16:39 -0600

    xgTexture: add function that calls bindTexture() of Graphics

--- GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.cpp
+++ GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.cpp
@@ -1,9 +1,15 @@
 #include "xgTexture.h"
 #include "../PString.h"
 #include "FilePointer.h"
+#include "Graphics.h"
 
 void xgTexture::load(FilePointer& file, const XG* xg)
 {
 	PString::ReadNamedValue("url", m_url, file);
 	PString::ReadNamedValue("mipmap_depth", m_mipmap_depth, file);
+}
+
+void xgTexture::bind() const
+{
+	Graphics::getGraphics()->bindTexture(m_url);
 }
--- GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.h
+++ GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.h
@@ -7,5 +7,6 @@ class xgTexture : public XG_SubNode
 	unsigned long m_mipmap_depth = 0;
 public:
 	void load(FilePointer& file, const XG* xg) override;
+	void bind() const;
 };
 
