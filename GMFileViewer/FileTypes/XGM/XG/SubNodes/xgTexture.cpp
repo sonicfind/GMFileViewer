2adac582e51a277f8b00f6054ffa0c798ee812d0
sonicfind <sonicfind2@gmail.com>
2023-01-21 00:26:30 -0600

    xgTexture: capitalize all texture names
    Allows for properly bound textures

--- GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.cpp
+++ GMFileViewer/FileTypes/XGM/XG/SubNodes/xgTexture.cpp
@@ -6,6 +6,12 @@
 void xgTexture::load(FilePointer& file, const XG* xg)
 {
 	PString::ReadNamedValue("url", m_url, file);
+	for (char& c : m_url)
+	{
+		if (!c)
+			break;
+		c = std::toupper(c);
+	}
 	PString::ReadNamedValue("mipmap_depth", m_mipmap_depth, file);
 }
 
