b73ee907767529b77dcc847a3b237ada4a642aaa
sonicfind <sonicfind2@gmail.com>
2023-01-24 11:24:49 -0600

    Pixel: add comparators

--- GMFileViewer/FileTypes/XGM/IMX/Pixel.h
+++ GMFileViewer/FileTypes/XGM/IMX/Pixel.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <compare>
 
 struct Pixel
 {
@@ -6,4 +7,34 @@ struct Pixel
 	unsigned char green;
 	unsigned char blue;
 	unsigned char alpha = 1;
+
+	friend auto operator<=>(const Pixel& lhs, const Pixel& rhs)
+	{
+		if (lhs.red != rhs.red)
+			return lhs.red <=> rhs.red;
+
+		if (lhs.green != rhs.green)
+			return lhs.green <=> rhs.green;
+
+		if (lhs.blue != rhs.blue)
+			return lhs.blue <=> rhs.blue;
+
+		return lhs.alpha <=> rhs.alpha;
+	}
+
+	friend bool operator==(const Pixel& lhs, const Pixel& rhs)
+	{
+		return lhs.red == rhs.red &&
+			lhs.green == rhs.green &&
+			lhs.blue == rhs.blue &&
+			lhs.alpha == rhs.alpha;
+	}
+
+	friend bool operator!=(const Pixel& lhs, const Pixel& rhs)
+	{
+		return lhs.red != rhs.red ||
+			lhs.green != rhs.green ||
+			lhs.blue != rhs.blue ||
+			lhs.alpha != rhs.alpha;
+	}
 };
