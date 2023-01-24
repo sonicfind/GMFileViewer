0e1b832b082d20f083a1f5c7cdf6aa4ce1fba629
sonicfind <sonicfind2@gmail.com>
2023-01-24 11:40:01 -0600

    XGEntry: add getters and setters
    + edit m_unknown for our own implementation

--- GMFileViewer/FileTypes/SSQ/XGEntry.h
+++ GMFileViewer/FileTypes/SSQ/XGEntry.h
@@ -6,7 +6,7 @@ class XGEntry
 	alignas(16) char m_name[16];
 	uint32_t m_isClone;
 	uint32_t m_cloneID;
-	uint32_t m_unknown;
+	uint32_t m_modelIndex;
 
 	SSQModelType m_type;
 	float m_length;
@@ -14,5 +14,10 @@ class XGEntry
 	float m_framerate;
 
 public:
+	std::string_view getName() const { return m_name; }
+	void setModelIndex(uint32_t index) { m_modelIndex = index; }
+	uint32_t getModelIndex() const { return m_modelIndex; }
+	bool isClone() const { return m_isClone; }
+	uint32_t getCloneID() const { return m_cloneID; }
 	SSQModelType getModelType() const { return m_type; }
 };
